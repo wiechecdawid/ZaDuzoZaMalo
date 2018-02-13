bce7a1cf2cad91fcd148191cbb69a77904620084
Dawid Wiecheć <d.wiechec@microsoft.wsei.edu.pl>
2018-02-13 17:21:55 +0100

    gra za dużo za mało

---
+++ GraZaDuzoZaMalo/Gra.Model/Gra.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraModel //logika gry
+{
+    public class Gra
+    {
+        readonly private int wylosowanaLiczba; //readonly jest metodą - może być użyty przy polach private
+        //przy projektowaniu można na chwilę zmienić na public, albo zrobić metodę w ramach klasy, aby wyświetlić priv.
+        public int MinZakres { get; } //private set; - mogę zmienić wewnątrz mojej klasy (readonly nie)
+        public int MaxZakres { get; }
+        //do zrobienia: czas gry, historia ruchów
+        public Status StatusGry { get; private set; }
+        public Odpowiedz Ocena(int propozycja)
+        {
+            if (propozycja < wylosowanaLiczba) return Odpowiedz.ZaMalo;
+            else if (propozycja == wylosowanaLiczba)
+            {
+                StatusGry = Status.Zakończona;
+                return Odpowiedz.Trafiony;
+            }
+            else return Odpowiedz.ZaDuzo;
+        }
+
+        public enum Odpowiedz { ZaMalo=-1, Trafiony=0, ZaDuzo=1} //stałe formalnie są wartościami liczbowymi
+
+        //public Gra() : this(1,100){} //mamy 1 wypasiony konstruktor, nad którym pracujemy i drugi, który przypisuje konkretne wartości
+
+        public Gra(int min=1, int max=100)
+        {
+            if (min >= max) throw new ArgumentException("min musi być mniejszy od max");
+            MinZakres = min;
+            MaxZakres = max;
+            /*Random rnd = new Random(); */ wylosowanaLiczba = (new Random()).Next(MinZakres, MaxZakres + 1);
+            StatusGry = Status.WTrakcie;
+        }
+        public int Poddaj()
+        {
+            StatusGry = Status.Poddana;
+            return wylosowanaLiczba;
+        }
+
+        //status gry
+        public enum Status { WTrakcie, Poddana, Zakończona }
+
+
+    }
+}
---
+++ GraZaDuzoZaMalo/GraConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraModel;
+
+namespace GraModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gra g = new Gra(1, 100);
+
+            do
+            {
+                while (true)
+                {
+                    Console.WriteLine("Podaj propozycję (x-poddaj się): ");
+                    string napis = Console.ReadLine();
+                    if ((napis.ToLower().Trim()).Equals("x")) //NAPISÓW NIE WOLNO PORÓWNYWAĆ ZA POMOCĄ ==
+                    {
+                        Console.WriteLine("wynik = {0}. KONIEC GRY", g.Poddaj());
+                        return; //funkcja Main nie zwraca wyniku więc nie musi zwracać liczby
+                        //Environment.Exit(1); - systemowe, bezwarunkowe wyjście z aplikacji
+                    }
+                    int propozycja = 0;
+                    try
+                    {
+                        propozycja = Int32.Parse(napis); //var zmusza do wywnioskowania typu (było ale trza było przenieść bo SCOPE)
+                    }
+                    //instrukcja sterująca catch - hierarchia wywoływania wyjątków LICZY SIĘ
+                    catch (FormatException) // mogę go nazwać, ale nie muszę
+                    {
+                        Console.WriteLine("nie przypomina liczby");
+                        continue;
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Przesadziłeś z wielkością liczby");
+                        continue;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("nieznany wyjątek");
+                        Console.WriteLine(e.StackTrace);
+                        continue;
+                    }
+
+                    Console.WriteLine(g.Ocena(propozycja));
+                    break;
+                }
+            }
+            while (g.StatusGry==Gra.Status.WTrakcie);
+            
+        }
+    }
+}
